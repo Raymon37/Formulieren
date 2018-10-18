3c2adead2e7f42792fac8620ca7a10709f5c3868
Raymon37 <43744360+Raymon37@users.noreply.github.com>
2018-10-18 16:55:38 +0200

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+
+<form action="welcome.php" method="post">
+Voornaam: <input type="text" name="name"><br>
+Achternaam: <input type="text" name="name"><br>
+Adres: <input type="text" name="adres"><br>
+Postcode: <input type="text" name="postcode"><br>
+E-mail: <input type="text" name="email"><br>
+Mobiel: <input type="text" name="mobiel"><br>
+<input type="submit">
+
+</form>
+</body>
+</html>
